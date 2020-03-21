3b9df153ca48a20506da7241645a1c2e067da17e
Sam Foster <sf00563@surrey.ac.uk>
2020-03-21 15:12:36 +0000

    haved added in devise and also added in firstname lastname plus have put in regex for surrey email

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'home#home'
-
+  devise_for :users
+  root to: 'home#home'
   get 'help', to: 'home#help'
   get 'contact', to: 'home#contact'
-
   get 'account', to: 'account#account'
-  
   get 'main', to: 'main#main'
-   
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
