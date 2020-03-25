260fdd1f1db36b4b4fa95f44b84a18346cd67b17
Wisdom <wn00094@surrey.ac.uk>
2020-03-25 21:29:31 +0000

    Basic features of the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'account', to: 'account#account'
   get 'main', to: 'main#main'
 
-
+  post 'request_contact', to: 'home#request_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
