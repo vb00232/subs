4de6a2c7334dd03291925cc5de5f11bc11f96708
Jack <jr00856@surrey.ac.uk>
2020-03-29 16:14:31 +0100

    added follow and tweet buttons

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2020_03_27_175817) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
