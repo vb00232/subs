a7242f4b075db71bb01ca1a70f9ef89186108952
Tom Wilson <tw00550@surrey.ac.uk>
2020-04-01 09:54:13 +0100

    Updated flash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,15 @@ class HomeController < ApplicationController
 
   def contact
   end
+
   def request_contact
     name = params[:name]
     email = params[:email]
     telephone = params[:telephone]
     message = params[:message]
+
+    # Checks if details are valid
+    valid = false
 
     if email.blank?
       flash[:alert] = I18n.t('home.request_contact.no_email')
@@ -19,8 +23,14 @@ class HomeController < ApplicationController
       flash[:alert] = I18n.t('home.request_contact.no_name')
     else
       flash[:notice] = I18n.t('home.request_contact.email_sent')
+      valid = true
     end
-
-    redirect_to root_path
+    # Redirects to the home page if email sent
+    if valid
+      redirect_to root_path
+    end
+    # Redirects back to contact page if email not sent
+    else
+      redirect_to contact_path
   end
 end
