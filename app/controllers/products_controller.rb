b75f560157fd0d51a0980eba247ff94191bb8a10
Tom Wilson <tw00550@surrey.ac.uk>
2020-04-16 21:33:44 +0100

    Added sort by nav bar to sellings page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,17 +6,43 @@ class ProductsController < ApplicationController
   # GET /products.json
 
   def index
+    # Keyword from search bar
     query = params[:query]
+    sort = params[:sort]
+
     # Checks if search query is present
     if query
-      @products = Product.search(params[:query], page: params[:page])
+      @products = Array.new
+
+      #Search based upon selected indexed fields
+      queryResults = Product.search_query(query)
+
+      # ElasticSearch returns products as JSON
+      # Retrieves products from the database using their id
+      queryResults.each do |result|
+        @products.append(Product.product_id(result.id).first)
+      end
+
+    # If no query, display all products
     else
       @products = Product.all
     end
+
     user = params[:user]
     if user
       @products = Product.user_products(user)
     end
+
+    if sort == t('sortby.lowtohigh')
+      @products = @products.order("products.price ASC")
+    elsif sort == t('sortby.hightolow')
+      @products = @products.order("products.price DESC")
+    elsif sort == t('sortby.newlistings')
+      @products = @products.order("products.created_at DESC")
+    elsif sort == t('sortby.oldlistings')
+        @products = @products.order("products.created_at ASC")
+    end
+
   end
 
   # def index
