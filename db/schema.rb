eee70b49e19328e112df7024cad5995f74ed85af
ds00718 <ds00718@surrey.ac.uk>
2020-04-17 05:29:23 -0700

    updated schema to reflect changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_120803) do
+ActiveRecord::Schema.define(version: 2020_04_17_122447) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 2020_04_17_120803) do
     t.integer "user_id", null: false
     t.integer "category"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "user_wishlists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_user_wishlists_on_product_id"
+    t.index ["user_id"], name: "index_user_wishlists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
