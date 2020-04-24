a9ec4640ce767fba5431da946c6416302f461206
Tom Wilson <tw00550@surrey.ac.uk>
2020-04-24 22:23:35 +0100

    Removed remote: true from product show wish list link as this prevents the page reloading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
       def after_sign_in_path_for(resource)
         stored_location_for(resource) || listings_path
       end
+      
       def favorite_text
       return @favorite_exists ? "Unfavorite" : "Unfavorite"
     end
