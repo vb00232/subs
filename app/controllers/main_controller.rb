aab7d8002a1dae696ab52772463616b23ca554d1
Tom Wilson <tw00550@surrey.ac.uk>
2020-05-02 14:49:14 +0100

    See all for a category now redirects to correct page layout, although layout itself needs changing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -26,55 +26,44 @@ class MainController < ApplicationController
     # Category filter
     cat = params[:category]
 
-    # # Checks for url parameters are present
-    # if query or cat
-    #   # Checks if search query is present
-    #   if query != ""
-    #     @products = Array.new
-    #
-    #     #Search based upon selected indexed fields
-    #     queryResults = Product.search_query(query)
-    #
-    #     # ElasticSearch returns products as JSON
-    #     # Retrieves products from the database using their id
-    #     queryResults.each do |result|
-    #       @products.append(Product.product_id(result.id).first)
-    #     end
-    #   # If no query, display all products
-    #   else
-    #     @products = Product.all
-    #   end
-    #
-    #   # Applies sorting to products
-    #   if sort == t('sortby.lowtohigh')
-    #     @products = @products.order("products.price ASC")
-    #   elsif sort == t('sortby.hightolow')
-    #     @products = @products.order("products.price DESC")
-    #   elsif sort == t('sortby.newlistings')
-    #     @products = @products.order("products.created_at DESC")
-    #   elsif sort == t('sortby.oldlistings')
-    #     @products = @products.order("products.created_at ASC")
-    #   end
-    #
-    #   # Checks that a category is present in url and that one has been selected
-    #   if cat && cat != t('categories.unselected')
-    #     # Finds the category record using the name passed in by the url
-    #     category_record = Category.find_by_name(cat).first
-    #     new_products = []
-    #     # Iterates through all products found so far
-    #     for product in @products
-    #       # Finds categories associated with each product
-    #       product_cats = ProductCategory.find_category(product)
-    #       for c in product_cats
-    #         if c.category == category_record
-    #           new_products.append(product)
-    #         end
-    #       end
-    #     end
-    #     @products = new_products
-    #   end
-    # # Default to displaying categories
-    if 1 == 1
+    # Checks for parameters
+    if(params.has_key?(:query) || params.has_key?(:category))
+      # Checks if search query is present
+      if query && query != ""
+        @products = Array.new
+
+        #Search based upon selected indexed fields
+        queryResults = Product.search_query(query)
+
+        # ElasticSearch returns products as JSON
+        # Retrieves products from the database using their id
+        queryResults.each do |result|
+          @products.append(Product.product_id(result.id).first)
+        end
+      # Displays all products if no query
+      else
+        @products = Product.all
+      end
+
+      # Checks that a category is present in url and that one has been selected
+      if cat && cat != t('categories.unselected')
+        # Finds the category record using the name passed in by the url
+        category_record = Category.find_by_name(cat).first
+        new_products = []
+        # Iterates through all products found so far
+        for product in @products
+          # Finds categories associated with each product
+          product_cats = ProductCategory.find_category(product)
+          for c in product_cats
+            if c.category == category_record
+              new_products.append(product)
+            end
+          end
+        end
+        @products = new_products
+      end
+    # If no parameters given, default to showing categories
+    else
       #Please feel free to optimise this -----added by Sam
       #wanted the most recent images to be displayed on main view listings page
       @productsNew = Product.new_items
@@ -87,6 +76,6 @@ class MainController < ApplicationController
       end
       @categories_products = categories_products
     end
-  end
 
+  end
 end
