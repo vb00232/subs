f443c2fc0b77c905ed56a54a46fd06aa5f4a8cfa
Tom Wilson <tw00550@surrey.ac.uk>
2020-06-09 20:54:34 +0100

    Updated fixtures, added tests for user and product models

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,51 +3,18 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @user = users(:one)
+    # Simulates a user signing in
+    get '/users/sign_in'
+    sign_in users(:one)
+    post user_session_url
   end
 
-
-=begin
   test "should get index" do
     get products_url
     assert_response :success
+    # Checks correct layout is used
+    assert_template layout: 'application'
   end
-
-  test "should get new" do
-    get new_product_url
-    assert_response :success
-  end
-
-  test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, name: @product.name, price: @product.price } }
-    end
-
-    assert_redirected_to product_url(Product.last)
-  end
-
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, name: @product.name, price: @product.price } }
-    assert_redirected_to product_url(@product)
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
-    end
-
-    assert_redirected_to products_url
-  end
-
-  =end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save invalid user' do
+    user = User.new
+    user.save
+    refute user.valid?
+  end
+
+  test 'should save valid user' do
+    user = User.new
+    user.firstname = "Rishab"
+    user.lastname = "Jain"
+    user.email = 'rj00338@surrey.ac.uk'
+    user.password = 'pass123'
+    user.save
+    assert user.valid?
+  end
 end
