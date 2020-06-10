6395ad5e9c0db21b2af19f38e3ef1f09d1866fa6
Tom Wilson <tw00550@surrey.ac.uk>
2020-06-10 14:44:13 +0100

    Updated seeds to add test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,23 +15,39 @@ Place.create!([
 { "name": "University of Surrey", "latitude": "51.2423","longitude": "0.5905"},
 ])
 
-# # Seeds used to test elastic search
-#
-# Product.__elasticsearch__.create_index!(force: true)
-#
-# # Test user
-# user1 = User.where(email: 'aa00000@surrey.ac.uk').first
-# user1.delete if user1
-# user1 = User.create([firstname: 'test',
-#   lastname: 'test',
-#   email: 'aa00000@surrey.ac.uk',
-#   password: 'password',
-#   encrypted_password: '#$taawktljasktlw4aaglj'])
-#
-# # Test product
-# product1 = Product.where(name: 'Test products').first
-# product1.delete if product1
-# product1 = Product.create([name: 'Name',
-#   description: 'Description',
-#   price: 1,
-#   user_id: user1.first.id])
+# Seeds used to test elastic search
+#Product.__elasticsearch__.create_index!(force: true)
+
+# Test users
+user1 = User.where(email: 'tw00001@surrey.ac.uk').first
+user1.delete if user1
+user1 = User.create([firstname: 'Tom',
+  lastname: 'Wilson',
+  email: 'tw00001@surrey.ac.uk',
+  admin: true,
+  password: 'password'])
+
+user2 = User.where(email: 'ab12345@surrey.ac.uk').first
+user2.delete if user2
+user2 = User.create([firstname: 'Alex',
+    lastname: 'Baker',
+    email: 'ab12345@surrey.ac.uk',
+    password: 'password'])
+
+user3 = User.where(email: 'rk00001@surrey.ac.uk').first
+user3.delete if user3
+user3 = User.create([firstname: 'Rahmi',
+        lastname: 'Khan',
+        email: 'rk00001@surrey.ac.uk',
+        password: 'password'])
+
+# Test products
+product1 = Product.where(name: 'Gaming chair').first
+product1.delete if product1
+product1 = Product.new
+product1.name = "Gaming chair"
+product1.price = 80
+product1.description = "A product for testing SUBS"
+product1.user = user1.first
+product1.save
+product1.image.attach(io: File.open('test/fixtures/files/TestImage.png'), filename: 'file.png')
